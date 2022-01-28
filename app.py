c06fa3914cc8c0974045c020517fa08035dd68ca
Anthony <amg568@drexel.edu>
2022-01-28 18:19:02 -0500

    add a basic form to the webapp, will change later

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/server', methods=['GET', 'POST'])
+def server():
+    if request.method == 'GET':
+        # if the status is 200, then send the data to the ML algorithm. Update the main page with a loading bar
+        # to show the progress on classification.
+        return '<p> GET </p>'
+    elif request.method == 'POST':
+        return '<p> POST </p>'
+    else:
+        abort(401)
+
+
+@app.route('/')
+def home_page():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
