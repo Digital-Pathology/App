bc5c94e6eca3b36ae3fdf8c852b49a829c3c6fcd
aw3334 <aw3334@drexel.edu>
2022-02-15 18:35:43 -0500

    fixing stuff

--- static/home.js
+++ static/home.js
@@ -7,20 +7,16 @@ function readURL(input) {
         $('.image-upload-wrap').hide();
   
         $('.file-upload-image').attr('src', e.target.result);
-        $('.file-upload-content').show();
-  
-        $('.image-title').html(input.files[0].name);
-      };
+      }
   
       reader.readAsDataURL(input.files[0]);
   
     } else {
       removeUpload();
     }
-}
+  }
   
-
-function removeUpload() {
+  function removeUpload() {
     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
     $('.file-upload-content').hide();
     $('.image-upload-wrap').show();
@@ -31,4 +27,3 @@ function removeUpload() {
       $('.image-upload-wrap').bind('dragleave', function () {
           $('.image-upload-wrap').removeClass('image-dropping');
   });
-  
