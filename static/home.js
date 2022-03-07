f8d8fbff7451de7552d50612d341d00f83fd069e
aw3334 <aw3334@drexel.edu>
2022-03-07 13:52:40 -0500

    styling homepage

--- static/home.js
+++ static/home.js
@@ -1,38 +1,27 @@
-function _(el) {
-    return document.getElementById(el);
-}
+const fileUploader = document.getElementById('file-uploader');
+const feedback = document.getElementById('feedback');
+const progress = document.getElementById('progress');
+
+const reader = new FileReader();
+
+fileUploader.addEventListener('change', (event) => {
+  const files = event.target.files;
+  const file = files[0];
+  reader.readAsDataURL(file);
   
-function uploadFile() {
-    var file = _("file1").files[0];
-    // alert(file.name+" | "+file.size+" | "+file.type);
-    var formdata = new FormData();
-    formdata.append("file1", file);
-    var ajax = new XMLHttpRequest();
-    ajax.upload.addEventListener("progress", progressHandler, false);
-    ajax.addEventListener("load", completeHandler, false);
-    ajax.addEventListener("error", errorHandler, false);
-    ajax.addEventListener("abort", abortHandler, false);
-    ajax.open("POST", "file_upload_parser.php"); // http://www.developphp.com/video/JavaScript/File-Upload-Progress-Bar-Meter-Tutorial-Ajax-PHP
-    //use file_upload_parser.php from above url
-    ajax.send(formdata);
-}
-
-function progressHandler(event) {
-    _("loaded_n_total").innerHTML = "Uploaded " + event.loaded + " bytes of " + event.total;
-    var percent = (event.loaded / event.total) * 100;
-    _("progressBar").value = Math.round(percent);
-    _("status").innerHTML = Math.round(percent) + "% uploaded... please wait";
-}
-
-function completeHandler(event) {
-    _("status").innerHTML = event.target.responseText;
-    _("progressBar").value = 0; //wil clear progress bar after successful upload
-}
-
-function errorHandler(event) {
-    _("status").innerHTML = "Upload Failed";
-}
-
-function abortHandler(event) {
-    _("status").innerHTML = "Upload Aborted";
-}
+  reader.addEventListener('progress', (event) => {
+    if (event.loaded && event.total) {
+      const percent = (event.loaded / event.total) * 100;
+      progress.value = percent;
+      document.getElementById('progress-label').innerHTML = Math.round(percent) + '%';
+      
+      if (percent === 100) {
+        let msg = `<span style="color:white;">File <u><b>${file.name}</b></u> has been uploaded successfully.</span>`;
+        if (files.length > 1) {
+            msg = `<span style="color:white;">Files have been uploaded successfully.</span>`;
+        }
+        feedback.innerHTML = msg;
+      }
+    }
+  });
+});
