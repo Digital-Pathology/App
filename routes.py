419512459fa8c1f47452309e78cfbf0e78495855
Kevin Karnani <kevinkarnani@gmail.com>
2022-03-22 06:08:37 +0000

    autopep

--- routes.py
+++ routes.py
@@ -10,16 +10,20 @@ from threading import Thread
 backend = Blueprint('server', __name__)
 home = Blueprint('home', __name__)
 
-ALLOWED_EXTENSIONS = {'tiff', 'svs', 'jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG'}
+ALLOWED_EXTENSIONS = {'tiff', 'svs', 'jpg',
+                      'jpeg', 'png', 'JPG', 'JPEG', 'PNG'}
+
 
 def allowed_file(File):
     return '.' in File.filename and File.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @home.route('/')
 @home.route('/home')
 def home_page():
     form = DocumentUploadForm()
     return render_template("home.html", form=form, loadingbar=False)
+
 
 @backend.route('/server', methods=['GET', 'POST'])
 def server():
@@ -32,9 +36,11 @@ def server():
             diagnosis_thread.start()
     return render_template("diagnosis.html")
 
+
 @backend.get('/modelStatus')
 def modelStatus():
     status_file = "diagnosis_status.json"
+
     def get_status():
         if not os.path.isfile(status_file):
             return {"error": "no status file"}
@@ -48,6 +54,7 @@ def do_diagnosis(path_to_image: str, model_name: str = "dummy_model"):
     print("Starting diagnosis function")
     status = {}
     status_file = "diagnosis_status.json"
+
     def publish_status():
         print("publishing diagnosis status")
         with open(status_file, 'w' if os.path.exists(status_file) else 'x') as f:
